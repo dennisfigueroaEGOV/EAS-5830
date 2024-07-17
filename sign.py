be90252da06fc8596713bfa279605c8445f15fa1
Dennis Figueroa <105014360+dennisfigueroaEGOV@users.noreply.github.com>
2024-07-16 22:56:31 -0500

    Update sign.py

--- sign.py
+++ sign.py
@@ -2,17 +2,16 @@ import eth_account
 from web3 import Web3
 from eth_account.messages import encode_defunct
 
-
 def sign(m):
     w3 = Web3()
-
+    
     account = eth_account.Account.create()
     eth_address = account.address
     private_key = account.privateKey
 
     message = encode_defunct(text=m)
-
-    signed_message = account.sign_message(message)
+    
+    signed_message = eth_account.Account.sign_message(message, private_key=private_key)
 
     assert isinstance(signed_message, eth_account.datastructures.SignedMessage)
 
