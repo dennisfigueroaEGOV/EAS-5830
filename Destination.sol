ce0f50a795d567a380396e904870ca31318ab6c8
Dennis Figueroa <105014360+dennisfigueroaEGOV@users.noreply.github.com>
2024-07-26 15:09:03 -0500

    Add files via upload

---
+++ Destination.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "./BridgeToken.sol";
+
+contract Destination is AccessControl {
+    bytes32 public constant WARDEN_ROLE = keccak256("BRIDGE_WARDEN_ROLE");
+    bytes32 public constant CREATOR_ROLE = keccak256("CREATOR_ROLE");
+	mapping( address => address) public underlying_tokens;
+	mapping( address => address) public wrapped_tokens;
+	address[] public tokens;
+
+	event Creation( address indexed underlying_token, address indexed wrapped_token );
+	event Wrap( address indexed underlying_token, address indexed wrapped_token, address indexed to, uint256 amount );
+	event Unwrap( address indexed underlying_token, address indexed wrapped_token, address frm, address indexed to, uint256 amount );
+
+    constructor( address admin ) {
+        _grantRole(DEFAULT_ADMIN_ROLE, admin);
+        _grantRole(CREATOR_ROLE, admin);
+        _grantRole(WARDEN_ROLE, admin);
+    }
+
+	function wrap(address _underlying_token, address _recipient, uint256 _amount ) public onlyRole(WARDEN_ROLE) {
+		//YOUR CODE HERE
+
+		address wrappedTokenAddress = underlying_tokens[_underlying_token];
+		
+		require(wrappedTokenAddress != address(0), "Underlying token not registered");
+
+		BridgeToken(wrappedTokenAddress).mint(_recipient, _amount);
+
+    	emit Wrap(_underlying_token, wrappedTokenAddress, _recipient, _amount);
+
+	}
+
+	function unwrap(address _wrapped_token, address _recipient, uint256 _amount ) public {
+
+		address underlyingTokenAddress = wrapped_tokens[_wrapped_token];
+    	require(underlyingToken != address(0), "Not a valid wrapped token");
+
+		BridgeToken(wrappedTokenAddress).burnFrom(msg.sender, _amount);
+
+		emit Unwrap(underlying_token, wrapped_token, msg.sender, _recipient, amount);
+
+
+	}
+
+	function createToken(address _underlying_token, string memory name, string memory symbol ) public onlyRole(CREATOR_ROLE) returns(address) {
+
+		BridgeToken newToken = new BridgeToken(name, symbol, symbol, address(this));
+		newToken.grantRole(newToken.MINTER_ROLE(), address(this));
+
+		address wrappedTokenAddress = address(newToken);
+
+		underlying_tokens[_underlying_token] = wrappedTokenAddress;
+		wrapped_tokens[wrappedTokenAddress] = _underlying_token;
+		tokens.push(wrappedTokenAddress);
+
+		emit Creation(underlying_tokens, wrapped_tokens);
+
+		return wrappedTokenAddress;
+	}
+
+}
+
+
